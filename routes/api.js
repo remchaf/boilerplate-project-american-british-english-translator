90b5390129ebd6ea27f2703fa038d2116c7f2241
remchaf <70279203+remchaf@users.noreply.github.com>
2022-06-13 03:37:58 +0000

    First commit !

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,34 @@
-'use strict';
+"use strict";
 
-const Translator = require('../components/translator.js');
+const Translator = require("../components/translator.js");
 
 module.exports = function (app) {
-  
   const translator = new Translator();
 
-  app.route('/api/translate')
-    .post((req, res) => {
-      
-    });
+  app.route("/api/translate").post((req, res) => {
+    const { text, locale } = req.body;
+
+    if (!text || !locale) {
+      res.json({
+        error: "Required field(s) missing",
+      });
+      return;
+    } else if (!text.length) {
+      res.json({
+        error: "No text to translate",
+      });
+      return;
+    } else if (
+      locale !== "american-to-british" &&
+      locale !== "british-to-american"
+    ) {
+      res.json({
+        error: "Invalid value for locale field",
+      });
+      return;
+    } else {
+      res.json({});
+      return;
+    }
+  });
 };
